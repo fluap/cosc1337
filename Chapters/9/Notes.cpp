9a36f652fd9e909be2d38d8d6f436f6df9e3c34f
fluap <4dfj8gg37a@protonmail.com>
2023-10-31 11:14:21 -0500

    Lab 5 assignment

---
+++ Chapters/9/Notes.cpp
@@ -0,0 +1,3 @@
+// Monday 25.10.2023 Chapter 8 Searching, Sorting, and Algorith, Analysis
+// slop lecture notes for chapter 9
+
---
+++ Lab Assignments/Faurie_Lab5/Faurie_Lab5.cpp
@@ -0,0 +1,125 @@
+#include<iostream>
+#include<iomanip>
+#include<string>
+
+#include<cmath>
+
+using namespace std;
+
+
+class waterConsumption {
+    private:
+        int monthly_consumption_amount[12];
+
+    public:
+        string months[12] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};;
+        //constructor
+
+        void input_data() {
+            int input;
+            for (int i = 0; i < 12; i++) {
+                cout << "Enter consumption for " << months[i] << ": ";
+                cin >> input;
+                //input validation
+                if (cin.fail() || input < 0) {
+                    cout << "That's not a valid number. \n";
+                    cin.clear();
+                    cin.ignore();
+                    i--; 
+                }
+                else {
+                    monthly_consumption_amount[i] = input;
+                }
+            }
+        }
+
+        void print_headers() {
+            int year;
+            cout << "Enter report year: ";
+            cin >> year;
+            if (cin.fail() || year < 2005 || year > 2020) {
+                cout << "Invalid year. Must be between 2005 and 2020. \n";
+                print_headers();
+            }
+            else {
+                cout << year << " Consumption Report for Paris, Texas \n\n";
+                cout << setw(22) << left << "Month";
+                cout << "Consumption in CCF \n";
+                cout << "****************************************\n";
+            }
+
+        }
+
+        void display_consumption() {
+            for (int i = 0; i < 12; i++) {
+                cout << setw(22) << left << months[i];
+                cout << monthly_consumption_amount[i] << "\n";
+            }
+        }
+
+        void get_lowest_consumption() {
+            int lowest = monthly_consumption_amount[0];
+            int lowest_index = 0;
+            for (int i = 0; i < 12; i++) {
+                if (monthly_consumption_amount[i] < lowest) {
+                    lowest = monthly_consumption_amount[i];
+                    lowest_index = i;
+                }
+            }
+            cout << "The lowest consumption was in " << months[lowest_index] << " with " << lowest << " CCF \n";
+        }
+
+        void get_highest_consumption() {
+            int highest = monthly_consumption_amount[0];
+            int highest_index;
+            for (int i = 0; i < 12; i++) {
+                if (monthly_consumption_amount[i] > highest) {
+                    highest = monthly_consumption_amount[i];
+                    highest_index = i;
+                }
+            }
+            cout << "The highest consumption was in " << months[highest_index] << " with " << highest << " CCF \n";
+        }
+
+        void get_average_consumption() { 
+            int average;
+            int total;
+            for (int i = 0; i < 12; i++) {
+                total += monthly_consumption_amount[i];
+            }
+
+            average = static_cast<float>(total) / 12; //Keeping as int due to sample output.
+            cout << "The total consumption for the year: " << total << " CCF \n";
+            cout << "The average consumption for the year: "<< fixed << setprecision(2) << average << " CCF \n";
+        }
+};
+
+
+int main(){
+    waterConsumption first_report;
+    first_report.input_data();              //1
+    first_report.print_headers();           //2
+    first_report.display_consumption();     //3
+    first_report.get_lowest_consumption();  //4
+    first_report.get_highest_consumption(); //5
+    first_report.get_average_consumption(); //6
+}
+
+// //Display consumption
+
+// void display_report(){
+//     true;
+// }
+
+// void display_highest(){
+//     true;
+// }
+
+// void display_lowest(){
+//     true;
+// }
+
+// void display_average(){
+//     // This function also displays the total
+//     true;
+// }
